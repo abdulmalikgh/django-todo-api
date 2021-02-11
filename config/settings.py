9e8b3e5817786e950be7e3e61ac9bf62a4a81e76
Musah Abdul-Malik <musah.abdulmalikgh@gmail.com>
2021-02-11 17:10:54 +0000

    Add test

--- config/settings.py
+++ config/settings.py
@@ -132,6 +132,6 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
 'DEFAULT_PERMISSION_CLASSES': [
-'rest_framework.permissions.AllowAny',
 ]
 }
+'rest_framework.permissions.AllowAny',
--- todos/tests.py
+++ todos/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import Todo
 
 # Create your tests here.
+
+class TodoModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Todo.objects.create(title="Test title", body="test body")
+
+    def test_title_content(self):
+        todo = Todo.objects.get(id=1)
+        title = f'{todo.title}'
+        self.assertEqual(title, 'Test title')
+
+    def test_body_content(self):
+        todo = Todo.objects.get(id=1)
+        body = f'{todo.body}'
+        self.assertEqual(body, 'test body')
